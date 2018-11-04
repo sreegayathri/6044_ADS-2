6282e2a5952c1e962bb52d25febde2923bce6e88
sreegayathri <sree0291@msitprogram.net>
2018-11-04 19:06:11 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/M 1.1   Graph/LinearProbingHashST.java
+++ assignments/M 1.1   Graph/LinearProbingHashST.java
@@ -82,25 +82,27 @@ public class LinearProbingHashST<Key, Value> {
     }
 
     /**
-     * Inserts the specified key-value pair into the symbol table, overwriting the old 
-     * value with the new value if the symbol table already contains the specified key.
-     * Deletes the specified key (and its associated value) from this symbol table
+     * Inserts the specified key-value pair into the symbol table,
+     * overwriting the old value with the new value if the symbol
+     * table already contains the specified key. Deletes the specified
+     * key (and its associated value) from this symbol table
      * if the specified value is {@code null}.
      *
      * @param  key the key
      * @param  val the value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void put(Key key, Value val) {
-        if (key == null) throw new IllegalArgumentException("first argument to put() is null");
-
+    public void put(final Key key, final Value val) {
+        if (key == null) { throw
+            new IllegalArgumentException("first argument to put() is null");
+        }
         if (val == null) {
             delete(key);
             return;
         }
 
         // double table size if 50% full
-        if (n >= m/2) resize(2*m);
+        if (n >= m / 2) { resize(2 * m); }
 
         int i;
         for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
@@ -121,24 +123,29 @@ public class LinearProbingHashST<Key, Value> {
      *         {@code null} if no such value
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public Value get(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to get() is null");
-        for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
-            if (keys[i].equals(key))
+    public Value get(final Key key) {
+        if (key == null) { throw 
+            new IllegalArgumentException("argument to get() is null");
+        }
+        for (int i = hash(key) { keys[i] != null; i = (i + 1) % m)
+            if (keys[i].equals(key)) {
                 return vals[i];
+            }
+        }
         return null;
     }
 
-    /**
-     * Removes the specified key and its associated value from this symbol table     
-     * (if the key is in this symbol table).    
-     *
+    /**.
+     * Removes the specified key and its associated
+     * value from this symbol table
+     * (if the key is in this symbol table).
      * @param  key the key
      * @throws IllegalArgumentException if {@code key} is {@code null}
      */
-    public void delete(Key key) {
-        if (key == null) throw new IllegalArgumentException("argument to delete() is null");
-        if (!contains(key)) return;
+    public void delete(final Key key) {
+        if (key == null) { throw 
+            new IllegalArgumentException("argument to delete() is null"); }
+        if (!contains(key)) { return; }
 
         // find position i of key
         int i = hash(key);
@@ -166,7 +173,7 @@ public class LinearProbingHashST<Key, Value> {
         n--;
 
         // halves size of array if it's 12.5% full or less
-        if (n > 0 && n <= m/8) resize(m/2);
+        if (n > 0 && n <= m / 8) { resize(m / 2); }
 
         assert check();
     }
@@ -180,26 +187,29 @@ public class LinearProbingHashST<Key, Value> {
      */
     public Iterable<Key> keys() {
         Queue<Key> queue = new Queue<Key>();
-        for (int i = 0; i < m; i++)
-            if (keys[i] != null) queue.enqueue(keys[i]);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) { queue.enqueue(keys[i]); }
+        }
         return queue;
     }
 
-    // integrity check - don't check after each put() because
-    // integrity not maintained during a delete()
+    /**. integrity check - don't check after each put() because
+    // integrity not maintained during a delete() */
     private boolean check() {
 
         // check that hash table is at most 50% full
         if (m < 2*n) {
-            System.err.println("Hash table size m = " + m + "; array size n = " + n);
+            System.err.println("Hash table size m = " +
+                m + "; array size n = " + n);
             return false;
         }
 
         // check that each key in table can be found by get()
         for (int i = 0; i < m; i++) {
-            if (keys[i] == null) continue;
+            if (keys[i] == null) { continue; }
             else if (get(keys[i]) != vals[i]) {
-                System.err.println("get[" + keys[i] + "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
+                System.err.println("get[" + keys[i] +
+                "] = " + get(keys[i]) + "; vals[i] = " + vals[i]);
                 return false;
             }
         }
