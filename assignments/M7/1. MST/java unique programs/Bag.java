cb383e0fea7699fe6ef57caa222114bb2b363cd8
sreegayathri <sree0291@msitprogram.net>
2018-11-05 20:46:43 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- assignments/M7/1. MST/java unique programs/Bag.java
+++ assignments/M7/1. MST/java unique programs/Bag.java
@@ -35,16 +35,16 @@ import java.util.NoSuchElementException;
  *  generic items. It supports insertion and iterating over the 
  *  items in arbitrary order.
  *  <p>
- *  This implementation uses a singly-linked list with a static nested class Node.
+ *  This implementation uses a singly-linked list with a
+ *  static nested class Node.
  *  See {@link LinkedBag} for the version from the
  *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
+ *  See {@link ResizingArrayBag} for a version that
+ *  uses a resizing array.
+ *  The <em>add</em>, <em>isEmpty</em>, and
+ *  <em>size</em> operations
+ *  take constant time. Iteration takes time
+ *   proportional to the number of items.
  *  @author Robert Sedgewick
  *  @author Kevin Wayne
  *
