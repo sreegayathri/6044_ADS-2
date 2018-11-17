03f1184954ff44542ca7f5ad2e6ae35926281703
sreegayathri <sree0291@msitprogram.net>
2018-11-17 12:23:56 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m18/ADS-2Exam-3/Solution.java
+++ assignments/m18/ADS-2Exam-3/Solution.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 /**
  * Class for solution.
  */
@@ -8,7 +8,7 @@ public final class Solution {
      */
     Solution() { }
     /**. Don't modify this method. */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String cases = scan.nextLine();
 
@@ -138,6 +138,7 @@ class T9 {
         for (String word : st.keys()) {
             text.put(word, st.get(word));
         }
+
     }
     /**.
      * Gets all words.
@@ -173,9 +174,60 @@ class T9 {
      */
     public Iterable<String> getSuggestions(Iterable<String> words, int k) {
         // your code goes here
+        // StringBuilder sequence = new StringBuilder();
+        // for (char c : text.toCharArray()) 
+        //     sequence.append(getDigit(c));
+        // return sequence.toString();
         return null;
     }
-
+// /**
+//  * Gets the digit.
+//  * @param      alphabet  The alphabet
+//  * @return     The digit.
+//  */
+//     public static char getDigit(char alphabet) {
+//         if (alphabet >= '0' && alphabet <= '9') {
+//             return alphabet;
+//         }
+//         switch (alphabet) {
+//         case 'a':
+//         case 'b':
+//         case 'c':
+//             return '2';
+//         case 'd':
+//         case 'e':
+//         case 'f':
+//             return '3';
+//         case 'g':
+//         case 'h':
+//         case 'i':
+//             return '4';
+//         case 'j':
+//         case 'k':
+//         case 'l':
+//             return '5';
+//         case 'm':
+//         case 'n':
+//         case 'o':
+//             return '6';
+//         case 'p':
+//         case 'q':
+//         case 'r':
+//         case 's':
+//             return '7';
+//         case 't':
+//         case 'u':
+//         case 'v':
+//             return '8';
+//         case 'w':
+//         case 'x':
+//         case 'y':
+//         case 'z':
+//             return '9';
+//         default:
+//             return '1';
+//         }
+//     }
     //final output
     //Don't modify this method.
     /**.
