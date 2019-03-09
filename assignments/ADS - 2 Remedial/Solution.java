722c587befa984e905f7d611a783744cae3afb00
sreegayathri <sree0291@msitprogram.net>
2019-03-09 11:04:50 +0530

    "ads_2_remedial_last: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/ADS - 2 Remedial/Solution.java
+++ assignments/ADS - 2 Remedial/Solution.java
@@ -9,7 +9,7 @@ class Solution {
         int circles  = Integer.parseInt(lines[0]);
         int routes  = Integer.parseInt(lines[1]);
         EdgeWeightedGraph ewgraph = new EdgeWeightedGraph(circles);
-        for (int i = 0; i < routes; i++) {
+        for (int i = 0; i < routes+1; i++) {
             String[] route = scan.nextLine().split(" ");
             Edge edg = new Edge(Integer.parseInt(route[0]),Integer.parseInt(route[1]),Integer.parseInt(route[2]));
             ewgraph.addEdge(edg);
