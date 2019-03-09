6d8ffba1fcd00ee7a44baa419049b7a8d774288b
sreegayathri <sree0291@msitprogram.net>
2019-03-09 11:19:08 +0530

    "ads_2_remedial_last: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/ADS - 2 Remedial/Solution.java
+++ assignments/ADS - 2 Remedial/Solution.java
@@ -1,32 +1,20 @@
 import java.util.Scanner;
-import java.lang.IllegalArgumentException;
-/**
- * Class for solution.
- * @author sreegayathri.
- */
-public final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() { }
-    /**
-     * { main function }.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String[] line = sc.nextLine().split(" ");
-        int vertices = Integer.parseInt(line[0]);
-        int edges = Integer.parseInt(line[1]);
-        EdgeWeightedGraph ewgraph = new EdgeWeightedGraph(vertices);
-        while (sc.hasNextLine()) {
-            String[] input = sc.nextLine().split(" ");
-            Edge edg = new Edge(Integer.parseInt(input[0]),
-                Integer.parseInt(input[1]), Double.parseDouble(input[2]));
+class Solution {
+    Solution() {
+        //
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] lines = scan.nextLine().split(" ");
+        int circles  = Integer.parseInt(lines[0]);
+        int routes  = Integer.parseInt(lines[1]);
+        EdgeWeightedGraph ewgraph = new EdgeWeightedGraph(circles);
+        for (int i = 0; i < routes-1; i++) {
+            String[] route = scan.nextLine().split(" ");
+            Edge edg = new Edge(Integer.parseInt(route[0]),Integer.parseInt(route[1]),Integer.parseInt(route[2]));
             ewgraph.addEdge(edg);
         }
         KruskalMST krush = new KruskalMST(ewgraph);
-        System.out.format("%.5f", krush.weight());
+        System.out.format("%d", krush.weight());
     }
 }
